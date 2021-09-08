35e68e3e7df1f55c2cd840076614c964ace1bfdd
Josias Henrique <joystickhenrii@gmail.com>
2021-09-08 15:48:27 -0300

    Layers

---
+++ backend/src/main/java/com/jooj/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.jooj.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jooj.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
